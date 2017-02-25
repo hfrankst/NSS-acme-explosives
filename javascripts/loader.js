91b7e621389fd8c272a6ce293de361d56784a623
Harper Frankstone <Harper Frankstone>
2017-02-25 16:19:44 -0600

    added some styling and wrapped functions around the promises, still having trouble getting the json data though

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -1,25 +1,76 @@
 "use strict";
 
+//arrays to store the corresponding json data
+let categoriesArray = [];
+let typesArray = [];
+let productsArray = [];
 
-	let privateExplosives = [];
-
+	
+function loadCategories() {
 	return new Promise((resolve, reject) => {
 			//XHR to load category json
-			let loader = new XMLHttpRequest();
-
-			loader.addEventListener("load", (responseText) => {
-				privateExplosives = JSON.parse(responseText);//jumping into the json where the objects are rather than the top level of the file
+			let catLoader = new XMLHttpRequest();
+			catLoader.addEventListener("load", (responseText) => {
 				console.log("ba boom");
-				console.log("privateExplosives", privateExplosives);
+				categoriesArray = JSON.parse(responseText);//jumping into the json where the objects are rather than the top level of the file
+				console.log("categoriesArray", categoriesArray);
 				resolve();	
 			});
 
-			loader.addEventListener("error", () => {
-				reject("load failed");
+			catLoader.addEventListener("error", () => {
+				reject("Loading the categories has failed.");
 			});
 
-			loader.open("GET", "json/products.json");
-			loader.send();
+			catLoader.open("GET", "json/categories.json");
+			catLoader.send();
 		});//end of promise
+}
+
+function loadTypes() {
+	
+	return new Promise((resolve, reject) => {
+		
+		let typeLoader = new XMLHttpRequest();
+
+		typeLoader.addEventListener("load", (responseText) => {
+			typesArray = JSON.parse(responseText);
+			console.log("typesArray", typesArray);
+			resolve();
+		}); 
+
+		typeLoader.addEventListener("error", () => {
+			reject("Loading the types has failed.");
+		});
+
+		typeLoader.open("GET", "json/types.json");
+		typeLoader.send();
+	});
+}
+
+function loadProducts() {
+	
+	return new Promise((resolve, reject) => {
+
+		let productLoader = new XMLHttpRequest();
+
+		productLoader.addEventListener("load", (responseText) => {
+			productsArray = JSON.parse(responseText);
+		});
+
+		productLoader.addEventListener("error", () => {
+			reject("Loading the products has failed.");
+		});
+
+		productLoader.open("GET", "json/products.json");
+		productLoader.send();
+	});
+}
+
+function cardBuilder(data) {
+	//use string literals combined with ${} selectors to specify the wanted values in the json
+}
 
 
+loadCategories();
+// loadTypes();
+// loadProducts();
